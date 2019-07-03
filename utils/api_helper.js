eaaf2dd9e3ff024983a01a8e6b87a26645ab347d
Sayed Saeed Hosseini <sayedsaeedhosseini@gmail.com>
2019-07-03 12:58:04 +0430

    feature (api helper): Implements api helper to get otp

---
+++ utils/api_helper.js
@@ -0,0 +1,24 @@
+import fetch from 'isomorphic-unfetch';
+
+export const getOtpCode = async (phone) => {
+  const request = fetch('https://pydtest.com/api/web/login/phone', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      phone
+    }),
+  });
+
+  const resp = await request;
+  let data;
+  if (resp.ok || resp.status < 500) {
+    data = await resp.json();
+  }
+  return {
+    ok: resp.ok,
+    status: resp.status,
+    data,
+  };
+};

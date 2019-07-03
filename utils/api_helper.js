c272c9fb20e1200818997f014fd6e76e8937dcb1
Sayed Saeed Hosseini <sayedsaeedhosseini@gmail.com>
2019-07-03 13:15:19 +0430

    feature (api helper): Implements api helper to submit otp code

--- utils/api_helper.js
+++ utils/api_helper.js
@@ -22,3 +22,27 @@ export const getOtpCode = async (phone) => {
     data,
   };
 };
+
+export const submitOtpCode = async (phone, code) => {
+  const request = fetch('https://pydtest.com/api/web/login/code', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      phone,
+      code,
+    }),
+  });
+
+  const resp = await request;
+  let data;
+  if (resp.ok || resp.status < 500) {
+    data = await resp.json();
+  }
+  return {
+    ok: resp.ok,
+    status: resp.status,
+    data,
+  };
+};

c062ef128d1145a8c9fdae46969ffb0b86639d20
Sayed Saeed Hosseini <sayedsaeedhosseini@gmail.com>
2019-07-10 20:34:14 +0430

    implement (listing): Checks user loged in and if it is false redirect it to login page

--- pages/listing.js
+++ pages/listing.js
@@ -1,24 +1,43 @@
 import React from "react";
+import Router from "next/dist/client/router";
 import Layout from '../components/Layout.js';
 
-function listing(result) {
-  return (
-    <Layout>
-      {
-        (result && result.ok) &&
-        result.data.data.map(
-          (item) => <p key={item._id}>{item.name}</p>
-        )
-      }
-      {
-        (result && !result.ok) &&
-        <p>
-          مشکلی پیش آمده است نازنینم.
-          {result.data.notification.message}
-        </p>
-      }
-    </Layout>
-  )
+class listing extends React.Component {
+
+  componentDidMount() {
+    const isUserLoggedIN = !!sessionStorage.getItem('accessToken');
+    if (!isUserLoggedIN) {
+      Router.push({
+        pathname: '/',
+      });
+      window.location.href = '/';
+    }
+  }
+
+  render() {
+    const { result } = this.props;
+    return (
+      <Layout>
+        {
+          (result && result.ok) &&
+          result.data.data.map(
+            (item) => <p key={item._id}>{item.name}</p>
+          )
+        }
+        {
+          (result && !result.ok) &&
+          <p>
+            مشکلی پیش آمده است نازنینم.
+            {
+              result.status >= 500
+                ? 'مشکلی در بر قراری ارتباط با سرور پیش آمده است. لطفا مجدد تلاش کنید.'
+                : result.data.notification.message
+            }
+          </p>
+        }
+      </Layout>
+    )
+  }
 }
 
 listing.getInitialProps = async ({ req }) => {
@@ -28,9 +47,11 @@ listing.getInitialProps = async ({ req }) => {
     data = await resp.json();
   }
   return {
-    ok: resp.ok,
-    status: resp.status,
-    data,
+    result: {
+      ok: resp.ok,
+      status: resp.status,
+      data,
+    }
   };
 };
 
